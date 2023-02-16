fb80e6c0a15b28b073de6a6e7ec9ac4c679e7cb9
Bahk Chanhee <bach@hey.com>
2023-02-17 00:56:16 +0900

    build: vercel edge test

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,6 +4,10 @@ import { env } from "../../../env.mjs";
 import { createTRPCContext } from "../../../server/api/trpc";
 import { appRouter } from "../../../server/api/root";
 
+export const config = {
+  runtime: "edge",
+};
+
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
@@ -12,7 +16,7 @@ export default createNextApiHandler({
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
           );
         }
       : undefined,
